e7712c72f7388162c9d1f7ad9479fdde9b5c423b
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-11-30 10:24:27 +0100

    fix icons

--- src/infrastructure/theme/index.ts
+++ src/infrastructure/theme/index.ts
@@ -1,11 +1,11 @@
 import {colors} from './colors'
 import {space} from './space'
 import {fonts} from './fonts'
-import {size} from './size'
+import {sizes} from './size'
 
 export const theme = {
     colors,
     fonts,
-    size,
+    sizes,
     space
 }
